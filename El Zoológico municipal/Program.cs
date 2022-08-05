da6ecd1d3b9c4e4e31c9432ae9ef0d814899d0e0
David Guzman Lopez <juan.guzman.ext@telefonica.com>
2022-08-05 09:10:41 -0500

    first commit

---
+++ El Zoológico municipal/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace El_Zoológico_municipal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+               Console.WriteLine("Ingrese el numero de animales");
+               int num_animales = int.Parse(Console.ReadLine());
+
+                string[] nombre = new string[num_animales];
+                string[] pais = new string[num_animales];
+                string[] especie = new string[num_animales];
+                string v1, v2, v3;
+
+                for (int x = 0; x <= num_animales-1; x++)
+                {
+                    Console.WriteLine("Nombre del animal");
+                    v1 = Console.ReadLine();
+                    nombre[x] = v1;
+
+                    Console.WriteLine("Pais de Origen");
+                    v2 = Console.ReadLine();
+                    pais[x] = v2;
+
+                    Console.WriteLine("Especie");
+                    v3 = Console.ReadLine();
+                    especie[x] = v3;
+
+                }
+
+                Console.WriteLine("Total de animales: " + num_animales);
+
+                for (int i = 0; i <= num_animales - 1; i++)
+                    { Console.WriteLine("\n\nNombre del animal: " + nombre[i] + "  Pais de origen: " + pais[i] + "  Especie: " + especie[i] ); }
+        }
+    }
+}
